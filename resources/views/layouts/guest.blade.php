d6cfeeba1426d13a661ebc3aae6521232b7605b1
alnomany <aalnomany50@gmail.com>
2026-07-04 13:16:50 +0300

    added google tag manager

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P553LFRT');</script>
+<!-- End Google Tag Manager -->
     @php
     function getFaviconPath($size, $restaurant) {
         $fileName = match($size) {
@@ -101,7 +107,10 @@
 </head>
 
 <body class="font-sans antialiased dark:bg-gray-900">
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P553LFRT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     <div class="mx-auto max-w-lg lg:max-w-screen-xl min-h-svh shadow-md lg:shadow-none">
         @livewire('shopNavigation', ['restaurant' => $restaurant, 'shopBranch' => $shopBranch])
