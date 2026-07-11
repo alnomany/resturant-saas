81632301f2c83a1c9ab035c4732ee9b4824f6495
alnomany <aalnomany50@gmail.com>
2026-07-11 14:48:30 +0300

    add function Coupon on website

--- database/migrations/2026_07_10_153723_add_discount_amount_to_orders_table.php
+++ database/migrations/2026_07_10_153723_add_discount_amount_to_orders_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
-           // $table->foreignId('coupon_id')->nullable()->constrained()->nullOnDelete();
+            ///
+            $table->foreignId('coupon_id')->nullable()->constrained()->nullOnDelete();
         });
     }
 
